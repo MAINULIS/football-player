2149ca1cc9d71811c9d4814c72bd0f4b86b64948
Mainul Islam <mainulislam6168@gmail.com>
2023-08-10 22:49:42 +0600

    player's cart added

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import './Players.css'
 
 const Players = (props) => {
+    const {img, name, price, age,country, ratings} =props.player
     return (
-        <div>
-            <img src="" alt="" />
+        <div className='players'>
+            <img src={img} alt="" />
             <div className='player-info'>
-                <h6>Name: {props.player.name}</h6>
+                <h5 className='players-name'>Name: {props.player.name}</h5>
+                <h6>Price: ${price}</h6>
+                <p>Country: {country}</p>
+                <p>Ratings: {ratings}star</p>
 
 
             </div>
